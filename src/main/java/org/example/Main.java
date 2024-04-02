46cfbe3e741f6bb55bf4678930c16a90fc91eb1b
Polina Kolesnikova <polina@example.com>
2024-04-03 00:59:52 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import com.google.gson.Gson;
+import org.example.domain.Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Ivan", "Ivanov", 30);
+        Person p2 = new Person("Ivan", "Ivanov", 30);
+        Person p3 = new Person("vasiliy", "Vasilyev", 35);
+        System.out.println(p1);
+        System.out.println(p3);
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.equals(p3));
+
+
+        // Сериализуем в  JSON
+
+        String result = serializeObjectToJSON(p1);
+
+        // Десериализуем JSON в объект
+        Person person = deSerializeObjectToJSON(result);
+
+        System.out.println(p1.equals(person));
+
+    }
+
+    private static Person deSerializeObjectToJSON(String jsonStr) {
+        Gson gson = new Gson();
+        Person person =gson.fromJson(jsonStr, Person.class);
+        System.out.println(person);
+        return person;
+    }
+
+    private static String serializeObjectToJSON(Person person) {
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+
+        return json;
+    }
+}
---
+++ src/main/java/org/example/domain/Person.java
@@ -0,0 +1,74 @@
+package org.example.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("age", age)
+                .toString();
+    }
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(firstName)
+                .append(lastName)
+                .append(age)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) obj;
+        return new EqualsBuilder()
+                .append(firstName, other.firstName)
+                .append(lastName, other.lastName)
+                .append(age, other.age)
+                .isEquals();
+    }
+}
